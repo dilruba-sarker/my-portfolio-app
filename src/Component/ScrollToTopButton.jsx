94a8830b67d2afd4419ffb59af4b8ae621010ecc
dilruba-sarker <dilrubasarkerseo@gmail.com>
2025-08-12 08:52:21 +0600

    scroll button

---
+++ src/Component/ScrollToTopButton.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import { FaArrowUp } from "react-icons/fa";
+
+const ScrollToTopButton = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  // Show button when scrolled down
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.scrollY > 300) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+    window.addEventListener("scroll", toggleVisibility);
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+
+  // Scroll to top
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    isVisible && (
+      <button
+        onClick={scrollToTop}
+        className="fixed bottom-6 right-6 p-3 bg-blue-600 text-white rounded-full shadow-lg hover:bg-blue-800 transition duration-300"
+      >
+        <FaArrowUp size={20} />
+      </button>
+    )
+  );
+};
+
+export default ScrollToTopButton;
--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,16 +1,39 @@
+// import React from 'react';
+// import { Outlet } from 'react-router';
+// import Navbar from '../Component/Navbar';
+// import Footer from '../Component/Footer';
+
+// const Root = () => {
+//     return (
+//         <div>
+//             <Navbar></Navbar>
+//      <div className='mt-20'> <Outlet></Outlet></div>
+       
+//         <Footer></Footer> 
+//         </div>
+//     );
+// };
+
+// export default Root;
+
+
 import React from 'react';
 import { Outlet } from 'react-router';
 import Navbar from '../Component/Navbar';
+import Footer from '../Component/Footer';
+import ScrollToTopButton from '../Component/ScrollToTopButton';
 
 const Root = () => {
     return (
-        <div className=' '>
-            <Navbar></Navbar>
-     <div className='mt-20'> <Outlet></Outlet></div>
+        <div>
+            <Navbar />
+            <div className='mt-20'>
+                <Outlet />
+            </div>
+            <Footer /> 
+            <ScrollToTopButton />
         </div>
     );
 };
 
 export default Root;
-
-
